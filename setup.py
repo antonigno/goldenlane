6a42e022ecdda7acd4e6ca39d219d5135fbaeae7
antonigno <stefano.borgia@gmail.com>
2014-11-18 15:05:49 +0000

    conf variables

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='goldenlane',
+    version='',
+    packages=['selenium', 'date-util', 'python-dateutil', 'pyvirtualdisplay'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
